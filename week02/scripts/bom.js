9d8cdedb95e745d70192adda366323a3558b5db0
MichaelKazembe <michaelkazembe@gmail.com>
2025-03-26 10:44:50 +0200

    initiate variables, create and populate a list, append to the list

---
+++ week02/scripts/bom.js
@@ -0,0 +1,20 @@
+//  Initiate the variables
+const input = document.querySelector('#favhap');
+const button = document.querySelector('button');
+const addList = document.querySelector('ul');
+
+// Create new list item and delete buttom
+const listItem = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+//  Populate the list item with the input value
+listItem.textContent = input.value;
+
+// Populate the delete button with an ❌
+deleteButton.textContent = '❌';
+
+// Append list item variable with delete button
+listItem.appendChild(deleteButton);
+
+// Append the list item to the unordered list
+addList.appendChild(listItem);
